bc17c0be81980deb056528e54d08848d8d3c8021
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-10 15:20:03 +0200

    Include logout URL

--- shopping_list/gui/urls.py
+++ shopping_list/gui/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('login', auth_views.LoginView.as_view())
+    path('login', auth_views.LoginView.as_view()),
+    path('logout', auth_views.LogoutView.as_view())
 ]
