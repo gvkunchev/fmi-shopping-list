c344ae94b1bb0e39636c6c4321f4de5f0d0f9945
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-11 15:41:46 +0200

    Add ShoppingList model - ensure that the database is updated:  $ python manage.py makemigrations  $ python manage.py migrate

--- shopping_list/gui/admin.py
+++ shopping_list/gui/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ShoppingItem
+from .models import ShoppingItem, ShoppingList
+
 
 class ShoppingItemAdmin(admin.ModelAdmin):
     model = ShoppingItem
@@ -8,4 +9,14 @@ class ShoppingItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'quantity', 'bought')
     fields = ('name', 'quantity', 'bought')
 
+
+class ShoppingListAdmin(admin.ModelAdmin):
+    model = ShoppingList
+    ordering = ('name', 'owner')
+    search_fields = ('name',)
+    list_display = ('name', 'owner')
+    fields = ('name', 'owner')
+
+
 admin.site.register(ShoppingItem, ShoppingItemAdmin)
+admin.site.register(ShoppingList, ShoppingListAdmin)
