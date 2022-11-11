be36230adf8ee977c318f5e6b5c514b2e64859f3
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-11 16:11:10 +0200

    Add meaningful representation for the models

--- shopping_list/gui/models.py
+++ shopping_list/gui/models.py
@@ -6,9 +6,14 @@ class ShoppingList(models.Model):
     name = models.CharField(max_length=90)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.owner.email} - {self.name}"
 
 class ShoppingItem(models.Model):
     name = models.CharField(max_length=90)
     quantity = models.IntegerField(default=1)
     bought = models.BooleanField(default=False)
     shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
