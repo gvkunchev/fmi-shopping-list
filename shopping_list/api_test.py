e1e9814ff39f265c70f37b679bfa2277c034f643
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-14 15:33:12 +0200

    Generate a token   $ python manage.py shell     >>from rest_framework_api_key.models import APIKey     >>api_key, key = APIKey.objects.create_key(name='mobile app')     >>key # 7LFZ9VrM.Ec2d76wKcMzN5DNJcU4YMw4OCxUwgbat  - Add the token in the header of the test file

--- shopping_list/api_test.py
+++ shopping_list/api_test.py
@@ -1,12 +1,15 @@
 import requests
 
-response = requests.get('http://localhost:8000/api/get_shopping_lists')
+response = requests.get('http://localhost:8000/api/get_shopping_lists',
+                        headers={'Authorization': 'Api-Key 7LFZ9VrM.Ec2d76wKcMzN5DNJcU4YMw4OCxUwgbat'})
 print(response.text)
 
 response = requests.post('http://localhost:8000/api/authenticate_user',
-                         json={'username': 'gosho', 'password': 'StrongPassword'})
+                         json={'username': 'gosho', 'password': 'StrongPassword'},
+                        headers={'Authorization': 'Api-Key 7LFZ9VrM.Ec2d76wKcMzN5DNJcU4YMw4OCxUwgbat'})
 print(response.text)
 
 response = requests.post('http://localhost:8000/api/authenticate_user',
-                         json={'username': 'gosho', 'password': 'WrongPassword'})
+                         json={'username': 'gosho', 'password': 'WrongPassword'},
+                        headers={'Authorization': 'Api-Key 7LFZ9VrM.Ec2d76wKcMzN5DNJcU4YMw4OCxUwgbat'})
 print(response.text)
