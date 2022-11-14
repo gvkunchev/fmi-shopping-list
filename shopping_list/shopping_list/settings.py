666268ecc36bca9bf26da0b46dbd3e4f42afd274
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-14 15:29:35 +0200

    Defend the API by using a token - Add the changes in sesttings.py - Make sure migrate in order to update the database accordingly (creating new table for tokens)  $ python manage.py makemigrations  $ python manage.py migrate

--- shopping_list/shopping_list/settings.py
+++ shopping_list/shopping_list/settings.py
@@ -132,3 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework_api_key.permissions.HasAPIKey",
+    ]
+}
