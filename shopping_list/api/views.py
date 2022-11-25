78a3f92969f83b1e150a046bcb3bf29f4d30a847
Georgi Kunchev <gvkunchev@gmail.com>
2022-11-25 12:04:43 +0200

    Introduce serializers

--- shopping_list/api/views.py
+++ shopping_list/api/views.py
@@ -2,13 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from gui.models import ShoppingList
 from django.contrib.auth import authenticate
-
+from .serializers import ShoppingListSerializer
 
 @api_view(['GET',])
 def get_shopping_lists(request):
     """Return list of shopping lists for a user."""
-    name_list = [x.name for x in ShoppingList.objects.all()]
-    return Response({'list': name_list})
+    serializer = ShoppingListSerializer(ShoppingList.objects.all(), many=True)
+    return Response(serializer.data)
 
 
 @api_view(['POST',])
